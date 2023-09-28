db12d4b663e2a0442816cdc51c3d6a07a8554687
linhey <is.linhey@outlook.com>
2023-09-28 15:30:18 +0800

    chore: 调整代码

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Qdrant",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v11), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)],
     products: [
         .library(name: "Qdrant", targets: ["Qdrant"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-http-types.git", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/apple/swift-http-types.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/Flight-School/AnyCodable", .upToNextMajor(from: "0.6.7"))
     ],
     targets: [
